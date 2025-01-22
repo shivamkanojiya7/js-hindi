b9d8f246c3cccf369cd41325c5672e94d59d5997
shivamkanojiya7 <shivam15705er@gmail.com>
2025-01-22 11:27:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+let name ="shivam"
+let age = 19
+let isLooggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // output= undefined
+console.log(typeof null); // output =object
