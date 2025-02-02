a62350b0d7b559454a8b3858aa7f66eef4b5b0c1
shivamkanojiya7 <shivam15705er@gmail.com>
2025-02-02 07:40:15 +0000

    container config done

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -11,7 +11,7 @@ for (const num of arr) {
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    console.log(`Each char is ${greet}`)
 }
 
 // Maps
